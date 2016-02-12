6fcdcb9c131f5017e83d313394a8f852d15a8f17
Lidia Fadina <fadina.lidiya@gmail.com>
2016-02-12 06:43:36 +0300

    Add selection_sort

---
+++ Sort_Array/selection_sort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define SIZE 5
+int main(void)
+{
+    char arr[SIZE] = {'c','b','d','a','e'};
+    int i,j,n;
+    char temp;
+    for(i = 0;i < SIZE; i++)
+        for(j = i; j < SIZE;j++){
+            if(*(arr + i) > *(arr + j + 1))
+            {
+                temp = *(arr + i);
+                *(arr + i) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
+            }
+        }
+      for(n = 0;n < SIZE; n++)
+                printf("%c",arr[n]);
+    return 0;
+}
