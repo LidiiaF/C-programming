acd42e0379577a84a8ed6cf7727481d58b67bfd5
Lidia Fadina <fadina.lidiya@gmail.com>
2016-02-14 10:25:39 +0300

    Add copy random values to array

---
+++ Arrays/copy_random_val.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define RANGE 10
+#define NUM 100
+int random_val(int n);
+void copy_val(int a[], int n, int m);
+void sort_ar(int a[],int n);
+void show_ar(int a[], int n);
+
+
+int main(void)
+{
+    int ar[NUM];
+
+    copy_val(ar,NUM,RANGE);
+    show_ar(ar,NUM);
+    putchar('\n');
+    putchar('\n');
+    sort_ar(ar,NUM);
+    show_ar(ar,NUM);
+
+    return 0;
+}
+int random_val(int n)
+{
+    int val;
+
+    val = rand()% n + 1;
+    return val;
+}
+void copy_val(int a[], int n, int m)
+{
+    int i;
+    for(i = 0; i < n; i++)
+        a[i] = random_val(m);
+}
+void show_ar(int a[], int n)
+{
+    int i;
+    for(i = 0; i < n; i++)
+        printf("%d ",a[i]);
+}
+void sort_ar(int a[], int n)
+{
+    int temp,i,j;
+    temp = 0;
+    for(i = 0;i < n; i++)
+        for(j = i; j < n;j++)
+            if(*(a + i) < *(a + j + 1))
+            {
+                temp = *(a + i);
+                *(a + i) = *(a + j + 1);
+                *(a + j + 1) = temp;
+            }
+}
